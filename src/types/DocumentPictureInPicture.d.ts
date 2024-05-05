701c497ac09a4422340f8354219127134ef6e815
aetheryx <mhzy.ali@gmail.com>
2024-05-05 14:06:47 +0000

    chore(types): describe `DocumentPictureInPictureOptions` options

--- src/types/DocumentPictureInPicture.d.ts
+++ src/types/DocumentPictureInPicture.d.ts
@@ -4,8 +4,11 @@
 type GlobalWindow = typeof window;
 
 interface DocumentPictureInPictureOptions {
+    /** Sets the initial width of the Picture-in-Picture window. */
     width?: number;
+    /** Sets the initial height of the Picture-in-Picture window. */
     height?: number;
+    /** Hides the "back to tab" button in the Picture-in-Picture window if true. It is false by default. */
     disallowReturnToOpener?: boolean;
 }
 
